bff92400c85b873621ab0626c6c2658469ee2323
Matt Anderson <Synse@users.noreply.github.com>
2022-11-10 21:23:51 +0000

    Cleanup embedded license

--- terror_zones.py
+++ terror_zones.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 """
+A Discord Bot for tracking Diablo 2: Resurrected Terror Zones - https://github.com/Martuck/TZ
+Copyright (C) 2022 @Synse, @Martuck
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
+
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
