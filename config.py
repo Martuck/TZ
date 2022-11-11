797d3cc0eaa29195c1eb01dcc867c710c5f7f911
Martuck <62221251+Martuck@users.noreply.github.com>
2022-11-11 07:53:33 -0600

    Create config.py
    Add instructions block.
    Add emoji function and all variables.
    Add roles function and all variables.

---
+++ config.py
@@ -0,0 +1,61 @@
+################
+# Instructions #
+################
+
+# Remove the hashtag "#" from the front of which ones you want to use and add your emoji or role id in the proper sections.
+# If the hashtag is left then the bot will use Discord Default Emoji or will not send a Role Ping
+# Example Emoji: #'Cold': ':cold_immune:', becomes 'Cold': ':myCustomEmote:',
+# Example Roles: #'Chaos Sanctuary': '12345', becomes 'Chaos Sanctuary': '12345',
+
+####################
+# End Instructions #
+####################
+
+def emoji():
+    emoji = {
+        #'Cold': ':cold_immune:',
+        #'Fire': ':fire_immune:',
+        #'Lightning': ':lightning_immune:',
+        #'Magic': ':magic_immune:',
+        #'Physical': ':physical_immune:',
+        #'Poison': ':poison_immune:',
+}
+
+def roles():
+    roles = {
+        #'Ancient\'s Way and Icy Cellar': 'ROLE_ID',
+        #'Arcane Sanctuary': 'ROLE_ID',
+        #'Arreat Plateau': 'ROLE_ID',
+        #'Blood Moor and Den of Evil': 'ROLE_ID',
+        #'Bloody Foothills': 'ROLE_ID',
+        #'Burial Grounds, The Crypt, and The Mausoleum': 'ROLE_ID',
+        #'Cathedral and Catacombs': 'ROLE_ID',
+        #'Chaos Sanctuary': 'ROLE_ID',
+        #'Cold Plains and The Cave': 'ROLE_ID',
+        #'Crystalline Passage and Frozen River': 'ROLE_ID',
+        #'Dark Wood': 'ROLE_ID',
+        #'Dry Hills and Halls of the Dead': 'ROLE_ID',
+        #'Durance of Hate': 'ROLE_ID',
+        #'Far Oasis': 'ROLE_ID',
+        #'Flayer Jungle and Flayer Dungeon': 'ROLE_ID',
+        #'Frigid Highlands': 'ROLE_ID',
+        #'Glacial Trail': 'ROLE_ID',
+        #'Jail': 'ROLE_ID',
+        #'Kurast Bazaar, Ruined Temple, and Disused Fane': 'ROLE_ID',
+        #'Kurast Sewers': 'ROLE_ID',
+        #'Lost City, Valley of Snakes, and Claw Viper Temple': 'ROLE_ID',
+        #'Moo Moo Farm': 'ROLE_ID',
+        #'Nihlathak\'s Temple, Halls of Anguish, Halls of Pain, and Halls of Vaught': 'ROLE_ID',
+        #'Outer Steppes and Plains of Despair': 'ROLE_ID',
+        #'River of Flame and City of the Damned': 'ROLE_ID',
+        #'Rocky Waste and Stony Tomb': 'ROLE_ID',
+        #'Sewers': 'ROLE_ID',
+        #'Spider Forest and Spider Cavern': 'ROLE_ID',
+        #'Stony Field': 'ROLE_ID',
+        #'Tal Rasha\'s Tombs and Tal Rasha\'s Chamber': 'ROLE_ID',
+        #'The Forgotten Tower': 'ROLE_ID',
+        #'The Pit': 'ROLE_ID',
+        #'Travincal': 'ROLE_ID',
+        #'Tristram': 'ROLE_ID',
+        #'Worldstone Keep, Throne of Destruction, and Worldstone Chamber': 'ROLE_ID',
+}
